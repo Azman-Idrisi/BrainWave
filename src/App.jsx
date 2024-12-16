bc31cd0f84e6f92e582d50750a6f36b8d39f3a55
Azman-Idrisi <idrisiazman@gmail.com>
2024-12-17 00:38:10 +0530

    Nav Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Button from './components/Button';
-import Header from './Header';
+import Header from './components/Header';
 
 const App = () => {
   return (
