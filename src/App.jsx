fad56a2ab46d13c2ed6c36beca87973383968383
Azman-Idrisi <idrisiazman@gmail.com>
2024-12-19 20:56:01 +0530

    pricing done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
 import Services from './components/Services';
+import Pricing from './components/Pricing';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
     <Services />
     </div>
   <ButtonGradient />
+  <Pricing />
   </>
   );
 }
