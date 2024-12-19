dc5896ef55e496e8f60f55c448db99c02c9d1583
Azman-Idrisi <idrisiazman@gmail.com>
2024-12-19 21:15:26 +0530

    Roadmap done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import ButtonGradient from './assets/svg/ButtonGradient'
-import Button from './components/Button';
-import Header from './components/Header';
-import Section from './components/Section';
-import Hero from './components/Hero';
-import Benefits from './components/Benefits';
-import Collaboration from './components/Collaboration';
-import Services from './components/Services';
-import Pricing from './components/Pricing';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import { Roadmap } from "./components/Roadmap";
+
 
 const App = () => {
   return (
-  <>
-    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-    <Header />
-    <Hero />
-    <Benefits />
-    <Collaboration />
-    <Services />
-    </div>
-  <ButtonGradient />
-  <Pricing />
-  </>
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+      </div>
+      <ButtonGradient />
+      <Pricing />
+      <Roadmap/>
+    </>
   );
-}
+};
 
 export default App;
